4d5c735559ce755dfee3830571c2ff6ce88a3735
novicheezy <novichdota@gmail.com>
2024-03-10 14:06:55 +0200

    Version 1.0

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Comment
+
+        /*
+        g
+        */
+
+        // Variables List: int
+        int numberOfstudents = 30;
+        int maxScore = 100;
+        System.out.println(numberOfstudents);
+        System.out.println(maxScore);
+
+        // Variables: double (дробные числа)
+        double temperature = 3.6;
+        double price = 999.99;
+        System.out.println(temperature);
+        System.out.println(price);
+
+        // Variables: Float
+        float pi = 3.14f;
+        float radius = 5.5f;
+        float area = pi + radius * radius;
+        System.out.println(area);
+
+        // Casting float to integer
+        int areaInteger = (int) area;
+        System.out.println(Math.round(area));
+
+        // Char
+        char symbol = '$';
+        System.out.println(symbol);
+        // Exercise:
+        char n = 78;
+        char i = 105;
+        char k = 107;
+        char t = 116;
+        char a = 97;
+        System.out.println(n);
+        System.out.println(i);
+        System.out.println(k);
+        System.out.println(i);
+        System.out.println(t);
+        System.out.println(a);
+
+        // Boolean
+        boolean isRaining = false;
+    }
+}
